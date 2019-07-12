8549442783469217a1f638f196fa5e7f04a0c194
Erik Espana <erikespana@users.noreply.github.com>
2019-07-12 09:21:11 -0500

    Create Footer.js

---
+++ 10-react-parent-child-component-practice/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Footer() {
+    return(
+        <footer>
+            <ol>
+                <li>footer item</li>
+            </ol>
+        </footer>
+    )
+}
+
+export default Footer
